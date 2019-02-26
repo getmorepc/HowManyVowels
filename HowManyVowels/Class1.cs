d87eea1ff26baccfb3091c4ee4edf7fe9d643934
Daniel Buchanan <daniel@getmorepc.com>
2019-02-26 14:37:56 -0500

    CountVowels

--- HowManyVowels/Class1.cs
+++ HowManyVowels/Class1.cs
@@ -4,5 +4,19 @@ namespace HowManyVowels
 {
     public class Class1
     {
+        //Create a function that takes a string and returns the number (count) of vowels contained within it.
+
+        public static int CountVowels(string str)
+        {
+            int a = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                {
+                    a++;
+                }
+            }
+            return a;
+        }
     }
 }
